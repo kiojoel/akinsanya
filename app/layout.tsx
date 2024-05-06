7fb4049717bdd386e185b84d7588a91303d66d69
Akinsanya Joel <akinsanyajoel82@.com>
2024-05-06 16:14:47 +0100

    fix screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
-          <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 ">
+          <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 h-screen ">
             {children}
           </main>
           <Footer />
