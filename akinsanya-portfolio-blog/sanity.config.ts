861c9b3c53daa6211b26871c9970132db7d7a327
Akinsanya Joel <akinsanyajoel82@.com>
2024-05-06 15:31:11 +0100

    remove vision

--- akinsanya-portfolio-blog/sanity.config.ts
+++ akinsanya-portfolio-blog/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemaTypes'
-import {visionTool} from '@sanity/vision'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: '2phg44xq',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
