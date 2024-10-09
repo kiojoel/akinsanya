e5af6c77cbb62680509a6803842dc52861c448a5
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-10-09 19:51:53 +0100

    minor changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex justify-between items-center w-full  py-4 px-8 text-[12px]">
       <div>{new Date().getFullYear()} @ All Rights Reserved</div>
-      <Link href="/">Akinsanya Joel</Link>
+      <Link href="/">Akinsanya</Link>
     </footer>
   );
 };
