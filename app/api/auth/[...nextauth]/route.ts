60539b0ebfe8c7b8e738b452bb368a0b08c2226d
Akinsanya Joel <akinsanyajoel82@gmail.com>
2025-07-02 18:47:23 +0100

    Implement full blog CMS

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,28 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, Profile } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { JWT } from "next-auth/jwt";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile) {
+        token.login = (profile as { login: string }).login;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.name = (token as { login: string }).login;
+      }
+      return session;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
